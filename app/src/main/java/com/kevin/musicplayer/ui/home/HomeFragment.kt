635185403c812a736461fd8df5ec1858d718cc97
Kevin Martin <kevin.martin@live.nl>
2018-12-17 18:00:31 +0100

    feature: tracklist in a tablayout

--- app/src/main/java/com/kevin/musicplayer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kevin/musicplayer/ui/home/HomeFragment.kt
@@ -1,37 +1,23 @@
 package com.kevin.musicplayer.ui.home
 
-import android.arch.lifecycle.Observer
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.kevin.musicplayer.R
 import com.kevin.musicplayer.base.BaseMVVMFragment
 import com.kevin.musicplayer.databinding.FragmentHomeBinding
-import com.kevin.musicplayer.model.Track
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : BaseMVVMFragment<FragmentHomeBinding, HomeViewModel>() {
 
-	private lateinit var trackListAdapter: TrackListAdapter
-	private val songList = ArrayList<Track>()
+	private lateinit var pagerAdapter : PagerAdapter
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-
-		trackListAdapter = TrackListAdapter(songList)
-		val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-
-		view.rvSongList.layoutManager = layoutManager
-		view.rvSongList.adapter = trackListAdapter
-		view.fastScroller.setRecyclerView(view.rvSongList)
-		viewModel.songs.observe(this, Observer { onDataSetChanged(it) })
-	}
-
-	private fun onDataSetChanged(tracks: List<Track>?) {
-		songList.clear()
-		tracks?.let { songList.addAll(tracks) }
-		trackListAdapter.notifyDataSetChanged()
+		pagerAdapter = PagerAdapter(activity!!.supportFragmentManager)
+		view.pager.adapter = pagerAdapter
+		view.pager.offscreenPageLimit = 2
+		view.tabLayout.setupWithViewPager(view.pager)
 	}
 
 	override fun initViewModelBinding() {
